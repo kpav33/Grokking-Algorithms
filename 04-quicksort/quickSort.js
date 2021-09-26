2f4c4d3b42b9295c571e62d7732494e0548b30f2
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-26 17:20:15 +0200

    Add quicksort pivot comment

--- 04-quicksort/quickSort.js
+++ 04-quicksort/quickSort.js
@@ -3,6 +3,12 @@ function quickSort(arr) {
   if (arr.length < 2) {
     return arr;
   } else {
+    // Performance of quicksort heavily depends on the pivot you choose (check book page 68)
+    // With a good pivot the best-case scenario performance is O(n * log n)
+    // Bad pivot can lead to worst-case scenario with performance O(n * n)
+    // If you choose a random element in the array as pivot, on average quicksort will have best-case scenario performance
+    // To avoid calculating random elements you could just pick the middle element as pivot by calculating array length divided with 2
+    // let pivot = arr[Math.floor(arr.length / 2)];
     let pivot = arr[0];
     let smaller = arr.filter((item) => item < pivot);
     let greater = arr.filter((item) => item > pivot);
